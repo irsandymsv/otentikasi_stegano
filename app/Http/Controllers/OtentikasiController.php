2a14268813d2110e90425661bb49d1016287cf35
irsandymsv <irsandymsv98@gmail.com>
2020-04-21 10:58:50 +0700

    menyiapkan route dan membuat OtentikasiController

---
+++ app/Http/Controllers/OtentikasiController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OtentikasiController extends Controller
+{
+   public function index()
+   {
+   	# code...
+   }
+
+   public function store_user()
+   {
+   	# code...
+   }
+
+	public function login()
+   {
+   	# code...
+   }
+
+   public function check_login()
+   {
+   	# code...
+   }
+
+   public function logout()
+   {
+   	# code...
+   }
+
+   public function pemulihan_gambar()
+   {
+   	# code...
+   }
+
+   public function kirim_email_pemulihan()
+   {
+   	# code...
+   }
+
+   public function reset_cover()
+   {
+   	# code...
+   }
+
+   public function update_cover()
+   {
+   	# code...
+   }
+
+   public function dashboard()
+   {
+   	# code...
+   }
+
+   public function download_cover()
+   {
+   	# code...
+   }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,15 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'OtentikasiController@index')->name('index');
+Route::post('store_user', 'OtentikasiController@store_user')->name('store_user');
+Route::get('/login', 'OtentikasiController@login')->name('login');
+Route::post('/login', 'OtentikasiController@check_login')->name('check_login');
+Route::get('/logout', 'OtentikasiController@logout')->name('logout');
+Route::get('/pemulihan_gambar', 'OtentikasiController@pemulihan_gambar')->name('pemulihan_gambar');
+Route::post('/kirim_email_pemulihan', 'OtentikasiController@kirim_email_pemulihan')->name('kirim_email_pemulihan');
+Route::get('/pemulihan_gambar/reset/{code}', 'OtentikasiController@reset_cover')->name('reset_cover');
+Route::post('/update_cover', 'OtentikasiController@update_cover')->name('update_cover');
+
+Route::get('/dashboard', 'OtentikasiController@dashboard')->name('dashboard');
+Route::get('/download_cover', 'OtentikasiController@download_cover')->name('download_cover');
